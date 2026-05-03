19cf65f88ae7a41563614331a72fab34ee972632
Sergey Leo Kallaur <sl@kallaur.ru>
2026-05-03 16:14:40 +0300

    переключился. Застрял на сборке дерева из массива

--- algos/leetcode/algo_101/algo.go
+++ algos/leetcode/algo_101/algo.go
@@ -19,14 +19,52 @@ func SymmetricTreeI(in []int, missVal int) bool {
 	if len(in)%2 != 1 {
 		return false
 	}
-	tree := BuildTree(in)
+	tree := BuildTree(in, missVal)
 	return false
 }
 
-func BuildTree(in []int) *Node {
-	root := &Node{
+// BuildTree собираем дерево из полученных данных
+// @param mv - передаем число которое обозначет пустые данные. Узел формироваться не будет
+func BuildTree(in []int, mv int) *Node {
+	nodesCounter := func(line int) int {
+		if line == 0 {
+			return 1
+		}
+		return line * 2
+	}
+
+	nodeBuilder := func(root *Node, l, r int, missVal int) *Node {
+		if l != missVal {
+			root.L = &Node{
+				Val: l,
+				L:   nil,
+				R:   nil,
+			}
+		}
+		if r != missVal {
+			root.R = &Node{
+				Val: r,
+				L:   nil,
+				R:   nil,
+			}
+		}
+		return root
+	}
+
+	var i int = 1
+	var line int = 1
+	var root *Node = &Node{
 		Val: in[0],
 		L:   nil,
 		R:   nil,
 	}
+
+	for i < len(in) {
+		nodes := nodesCounter(line)
+		for n := 0; n <= nodes; n++ {
+			root = nodeBuilder(root, in[i+1], in[i+2], mv)
+		}
+		line++
+	}
+	return root
 }

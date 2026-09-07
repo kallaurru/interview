451e898351dc3523430a684e7a0ce93b04a65b63
Sergey Leo Kallaur <sl@kallaur.ru>
2026-09-07 13:46:11 +0300

    добавил новые решения

--- examples/highload/p7-db/p7-02-index/main.go
+++ examples/highload/p7-db/p7-02-index/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -18,9 +19,8 @@ func main() {
 		fmt.Printf("Error - %v", err)
 		os.Exit(1)
 	}
-	head := make(map[string][]string, k)
+	idxList := make([]string, 0, k)
 	idxItems := make(map[string][]string, k)
-	cache := make(map[string]string, k)
 
 	for i := 0; i < k; i++ {
 		var idxName, idxPartsLine string
@@ -30,20 +30,10 @@ func main() {
 			fmt.Printf("Error - %v", err)
 			os.Exit(1)
 		}
-
-		cache[idxPartsLine] = idxName
-		idxParts := strings.Split(idxPartsLine, ",")
-		if len(idxParts) < 1 {
-			continue
-		}
-		idxlist, ok := head[idxParts[0]]
-		if !ok {
-			idxlist = make([]string, 0, 2)
-		}
-		idxlist = append(idxlist, idxName)
-		head[idxParts[0]] = idxlist
-		idxItems[idxName] = idxParts
+		idxList = append(idxList, idxName)
+		idxItems[idxName] = strings.Split(idxPartsLine, ",")
 	}
+	sort.Slice(idxList, func(i, j int) bool { return idxList[i] < idxList[j] })
 
 	for i := 0; i < q; i++ {
 		var qPartsLine string
@@ -57,20 +47,7 @@ func main() {
 		if len(items) == 0 {
 			continue
 		}
-		idx, ok := cache[qPartsLine]
-		if ok {
-			// префикс совпал полностью
-			fmt.Printf("%s %d\n", idx, len(idxItems[idx]))
-			continue
-		}
-		// первое поле префикса
-		idxs, ok := head[items[0]]
-		if !ok {
-			// индексы по указанному префиксу не найдены
-			fmt.Printf("%s\n", "FULLSCAN")
-			continue
-		}
-		fit(idxs, idxItems, items)
+		fit(idxList, idxItems, items)
 	}
 }
 
@@ -79,16 +56,19 @@ func fit(idxlist []string, idxItems map[string][]string, qFields []string) {
 	idxNameEnd := ""
 
 	for _, idxName := range idxlist {
-		prefixParts := idxItems[idxName]
+		items := idxItems[idxName]
 		counter := 0
-		// один проход будет всегда, потому что мы нашли список индексов по первому полю
-		for idx := 0; idx < len(qFields); idx++ {
-			if prefixParts[idx] != qFields[idx] {
-				break
+		for _, qItem := range items {
+			flg := false
+			for idx := 0; idx < len(qFields); idx++ {
+				if qItem == qFields[idx] {
+					flg = true
+					counter++
+					break
+				}
 			}
-			counter++
-			if idx+1 == len(prefixParts) {
-				break // закончились поля префикса
+			if !flg {
+				break // прекращаем поиск
 			}
 		}
 		if counter > maxPrefix {
